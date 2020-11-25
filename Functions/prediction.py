267cfdfb96885308faff199f2a07bb799789a7e2
PengJinEd2020 <s2087060@ed.ac.uk>
2020-11-25 16:30:14 +0000

    1

---
+++ Functions/prediction.py
@@ -0,0 +1,8 @@
+def pred_for_demand(period[  ]/month):
+
+    read_data_monthly(path)
+
+    return station_list, demand
+
+
+def pred_for_demand():
---
+++ Functions/read_data.py
@@ -0,0 +1,4 @@
+def read_data_monthly(path):
+    ###
+    return station_list, array(day,stations,value=end_point),
+    array(day,value=num_of_flow), array(flow,value=duration)
